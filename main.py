532335901ac5756e4e2fbdd2e2426268df9568a4
Brent Kong <brentkong21@gmail.comgit config --global user.email brentkong21@gmail.com>
2022-09-24 19:42:47 -0700

    revised odd function

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ print("git add, git commit, git push")
 def is_even(num):
     return num % 2 == 0
 
-def is_0dd(num):
+def is_odd(num):
     return not is_even(num)
+
+print(is_odd(2))
     
 print(is_even(2))
